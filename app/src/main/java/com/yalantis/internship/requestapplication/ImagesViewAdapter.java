156c7b8ecbfa19d7f7417610eb969bdc1436e7de
Sergey Voronov <sergey@voronov.kiev.ua>
2016-03-31 20:22:41 +0300

    edited by the comments of the code

--- app/src/main/java/com/yalantis/internship/requestapplication/ImagesViewAdapter.java
+++ app/src/main/java/com/yalantis/internship/requestapplication/ImagesViewAdapter.java
@@ -1,73 +1,68 @@
 package com.yalantis.internship.requestapplication;
 
+import com.bumptech.glide.Glide;
+
 import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
-import com.bumptech.glide.Glide;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class ImagesViewAdapter extends RecyclerView.Adapter<ImagesViewAdapter.ImageHolder> {
 
-  private final List<ImageModel> mImages;
+    private final String[] imagesDataSet;
 
-  public ImagesViewAdapter(List<ImageModel> images) {
-    this.mImages = images; //[Comment] Without this
-  }
-
-  @Override
-
-  public ImageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    View itemView = LayoutInflater.
-        from(parent.getContext()).
-        inflate(R.layout.request_images_layout, parent, false);
-
-    return new ImageHolder(itemView); //[Comment] Wrong formatting
-  }
-
-  @Override public void onBindViewHolder(final ImageHolder holder, int position) {
-    final ImageModel imageModel = mImages.get(position);
-
-    Resources r = holder.imageView.getResources();
-    int px = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, r.getDisplayMetrics()); //[Comment] Magic number
-    Glide.with(holder.imageView.getContext())
-        .load(imageModel.getUrl())
-        .override(px, px)
-        .centerCrop()
-        .into(holder.imageView);
-    holder.imageView.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-
-        Pattern p = Pattern.compile(".*:id\\/(.*)");
-        String s = holder.imageView.getContext().getResources().getResourceName(v.getId());
-        Matcher m = p.matcher(s);
-        if (m.find()) s = m.group(1);
-        Toast toast = Toast.makeText(holder.imageView.getContext(), s + "-" + imageModel.getUrl(),
-            Toast.LENGTH_SHORT);
-        toast.show();
-      }
-    });
-
-    //  holder.imageView.setImageResource(imageModel.getId()); //[Comment] Commented code
-  }
-
-  @Override public int getItemCount() {
-    return mImages.size();
-  }
-
-  public static class ImageHolder extends RecyclerView.ViewHolder {
-    final ImageView imageView; //[Comment] Wrong visibility modifier
-
-    public ImageHolder(View v) {
-      super(v);
-      imageView = (ImageView) v.findViewById(R.id.imgRequest);
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public ImagesViewAdapter(String[] imagesDataSet) {
+        this.imagesDataSet = imagesDataSet;
     }
-  }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ImageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.
+                from(parent.getContext()).
+                inflate(R.layout.request_images_layout, parent, false);
+        return new ImageHolder(itemView); //[Comment] Wrong formatting
+    }
+
+    @Override
+    public void onBindViewHolder(final ImageHolder holder, int position) {
+        final String imageUrl = imagesDataSet[position];
+
+        Resources r = holder.imageView.getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, r.getDimension(R.dimen.image_size), r.getDisplayMetrics());
+        Glide.with(holder.imageView.getContext())
+                .load(imageUrl)
+                .override(px, px)
+                .centerCrop()
+                .into(holder.imageView);
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(holder.imageView.getContext(),
+                        v.getClass().getSimpleName(),
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagesDataSet.length;
+    }
+
+    public static class ImageHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView; //[Comment] Wrong visibility modifier
+
+        public ImageHolder(View v) {
+            super(v);
+            imageView = (ImageView) v.findViewById(R.id.imgRequest);
+        }
+    }
 }
--- app/src/main/java/com/yalantis/internship/requestapplication/RequestViewActivity.java
+++ app/src/main/java/com/yalantis/internship/requestapplication/RequestViewActivity.java
@@ -5,96 +5,89 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-//[Comment] Wrong paddings
-//[Comment] Wrong toolbar and status bar color
-//[Comment] Wrong "In worj label
+//look at issue please:)
+
 public class RequestViewActivity extends AppCompatActivity {
+    //array activity views
+    //on click listener views. show toast with id name
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(final View v) {
+            Toast toast = Toast.makeText(getApplicationContext(), v.getClass().getSimpleName(), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    };
 
-  //array of urls images
-  private final List<ImageModel> mImagesList = new ArrayList<>();
-  //array activity views
-  private final ArrayList<View> mViews = new ArrayList<>(); //[Comment] Use abstraction instead of realization
-
-
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_request_layout);
-    setupUpActionPanel();
-    showImages();
-
-    //get activity viewgroup
-    ViewGroup viewGroup = (ViewGroup) findViewById(android.R.id.content);
-    findViews(viewGroup);
-
-    //add onclick listener views in activity
-    for (View v : mViews) {
-      v.setOnClickListener(onClickListener);
-      Log.v(getString(R.string.app_name), getResources().getResourceName(v.getId()));
-    }
-  }
-
-  //on click listener views. show toast with id name
-  private final View.OnClickListener onClickListener = new View.OnClickListener() {
-    @Override public void onClick(final View v) {
-      Pattern p = Pattern.compile(".*:id\\/(.*)"); //[Comment] Bad idea. Use view.getClass().getSimpleName
-      String s = getResources().getResourceName(v.getId());
-      Matcher m = p.matcher(s);
-      if (m.find()) s = m.group(1);
-      Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
-      toast.show();
-    }
-  };
-
- //find all views in view group
-  private void findViews(ViewGroup viewGroup) {
-    for (int i = 0, N = viewGroup.getChildCount(); i < N; i++) {
-      View child = viewGroup.getChildAt(i);
-      if (child instanceof ViewGroup) {
-        findViews((ViewGroup) child);
-      } else {
-        mViews.add(child);
-      }
-    }
-  }
-
-  //set up actionbar(add home button, and title)
-  private void setupUpActionPanel() {
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar!=null) {
-      actionBar.setHomeButtonEnabled(true); //[Comment] Wrong formatting
-      actionBar.setDisplayHomeAsUpEnabled(true);
-      actionBar.setHomeAsUpIndicator(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
-      actionBar.setTitle(getString(R.string.request_number));
-    }
-  }
-
-  //home(up) event action
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    finish();
-    return super.onOptionsItemSelected(item); //[Comment] Wrong formatting
-  }
-  //recycleView  show
-  private void showImages() {
-    String[] pics_urls = getResources().getStringArray(R.array.pic_urls);
-    for (String u : pics_urls) {
-      mImagesList.add(new ImageModel(u));
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_layout);
+        showImages();
+        ViewGroup viewGroup = (ViewGroup) findViewById(android.R.id.content);
+        ArrayList<View> mViews = new ArrayList<>();
+        setupUpActionPanel();
+        mViews = findViews(viewGroup, mViews);
+        //add onclick listener event views in activity
+        for (View v : mViews) {
+            v.setOnClickListener(onClickListener);
+        }
+        setupUpActionPanel();
     }
 
-    ImagesViewAdapter imageViewAdapter = new ImagesViewAdapter(mImagesList);
-    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.images_view);
-    LinearLayoutManager linearLayoutManager =
-        new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+    //find all views in view group
+    private ArrayList<View> findViews(ViewGroup viewGroup, ArrayList<View> views) {
+        for (int i = 0, N = viewGroup.getChildCount(); i < N; i++) {
+            View child = viewGroup.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                findViews((ViewGroup) child, views);
+            } else {
+                views.add(child);
+            }
+        }
+        return views;
+    }
 
-    recyclerView.setAdapter(imageViewAdapter);
-    recyclerView.setLayoutManager(linearLayoutManager);
-  }//[Comment] Wrong formatting
+    //set up actionbar(add home button, and title)
+    private void setupUpActionPanel() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle(getString(R.string.request_number));
+            setSupportActionBar(toolbar);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(this, "ImageButton", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //recycleView  show
+    private void showImages() {
+        String[] pics_urls = getResources().getStringArray(R.array.pic_urls);
+        ImagesViewAdapter imageViewAdapter = new ImagesViewAdapter(pics_urls);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.images_view);
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setAdapter(imageViewAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
 }
